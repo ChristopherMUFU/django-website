faea11ee4e34fa3443dc84829ee46c9fe3310dc2
ChristopherMUFU <christopher.mufu@gmail.com>
2022-11-30 06:54:58 +0100

    debug true

--- site_back/settings.py
+++ site_back/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b21d+hp95j)cnv7glybgc4xl!5btn1ta^hiugzfd=e+!p!s&#m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
